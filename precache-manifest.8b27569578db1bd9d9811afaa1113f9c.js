9fec529250779fb74caa8d9fa2bd3d11a4d4fa4c
thomashexton <tom.hexton@gmail.com>
2019-07-16 13:27:34 +1000

    Updates

---
+++ precache-manifest.8b27569578db1bd9d9811afaa1113f9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e762b6c3925901454949cae2e344e25b",
+    "url": "/palo_spa_client/index.html"
+  },
+  {
+    "revision": "215366fc3bcf1f6ebaff",
+    "url": "/palo_spa_client/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "e6d169cc3857bf1e07dd",
+    "url": "/palo_spa_client/static/js/2.d5b7ab4a.chunk.js"
+  },
+  {
+    "revision": "215366fc3bcf1f6ebaff",
+    "url": "/palo_spa_client/static/js/main.6a64ba99.chunk.js"
+  },
+  {
+    "revision": "5868cebb4b1aae559f31",
+    "url": "/palo_spa_client/static/js/runtime~main.c4b1cd3a.js"
+  }
+]);
