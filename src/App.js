ac9521d5ae8a59409e063817a8c74703aadcc8b3
thomashexton <tom.hexton@gmail.com>
2019-07-17 12:03:56 +1000

    updated SERVER_URL to correct one

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ import About from './components/pages/About';
 
 import axios from 'axios';
 
-const SERVER_URL = 'http://localhost:3000';
-// const SERVER_URL = 'https://palo-spa-server.herokuapp.com';
+// const SERVER_URL = 'http://localhost:3000';
+const SERVER_URL = 'https://palo-spa-server.herokuapp.com';
 
 class App extends Component {
 	componentDidMount() {
